ead234452707c81bab7106f4808bd9637cad6bca
keerti <keerti.mili@gmail.com>
2020-05-12 18:41:49 -0400

    frequency & monetary

--- RFM.py
+++ RFM.py
@@ -30,3 +30,61 @@ plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
+
+
+kmeans=KMeans(n_clusters=4 , init='k-means++')
+kmeans.fit(users[['Recency']])
+users['RecencyCluster']=kmeans.predict(users[['Recency']])
+
+def order_cluster(cluster_name,target_name,data,ascending):
+    data_new = data.groupby(cluster_name)[target_name].mean().reset_index()
+    data_new = data_new.sort_values(by=target_name,ascending=ascending).reset_index(drop=True)
+    data_new['index'] = data_new.index
+    data_final = pd.merge(data,data_new[[cluster_name,'index']], on=cluster_name)
+    data_final = data_final.drop([cluster_name],axis=1)
+    data_final = data_final.rename(columns={"index":cluster_name})
+    return data_final
+users = order_cluster('RecencyCluster', 'Recency',users,True)
+
+users.groupby('RecencyCluster')['Recency'].describe()
+
+###################################################################################
+# Calculating Frequency
+
+frequency=uk.groupby(by='CustomerID').InvoiceDate.count().reset_index()
+frequency.columns=['CustomerID','Frequency']
+
+users=pd.merge(users,frequency,on='CustomerID')
+
+kmeans=KMeans(n_clusters=4 , init='k-means++')
+kmeans.fit(users[['Frequency']])
+users['FrequencyCluster']=kmeans.predict(users[['Frequency']])
+
+users = order_cluster('FrequencyCluster', 'Frequency',users,True)
+
+users.groupby('FrequencyCluster')['Frequency'].describe()
+
+
+###################################################################################
+
+#Calculating Monetary
+
+
+monetary_value = uk.groupby('CustomerID').MonetaryValue.sum().reset_index()
+monetary_value.columns=['CustomerID','Monetary']
+users = pd.merge(users, monetary_value, on='CustomerID')
+
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(users[['Monetary']])
+users['MonetaryCluster'] = kmeans.predict(users[['Monetary']])
+
+users = order_cluster('MonetaryCluster', 'Monetary',users,True)
+
+users.groupby('MonetaryCluster')['Monetary'].describe()
+
+users['OverallScore'] = users['RecencyCluster'] + users['FrequencyCluster'] + users['MonetaryCluster']
+users.groupby('OverallScore')['Recency','Frequency','Monetary'].mean()
+
+users['Segment'] = 'Low-Value'
+users.loc[users['OverallScore'] > 2,'Segment'] = 'Mid-Value' 
+users.loc[users['OverallScore'] > 4,'Segment'] = 'High-Value'
